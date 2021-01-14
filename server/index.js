9959010215723ac2f50d05200daafb56c140f09e
Brandon Nguyen <bnguyenlps@gmail.com>
2021-01-14 11:21:10 -0800

    Inital environment and seed setup (copied from previous repo)

---
+++ server/index.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-console */
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('api/img_carousel');
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
