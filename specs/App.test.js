1784e78ba2b6c34bb4ea652eac9be50cf19f48cb
Brandon Nguyen <bnguyenlps@gmail.com>
2021-01-16 11:27:28 -0800

    set up tests to check my get request to see if I'm getting something

--- specs/App.test.js
+++ specs/App.test.js
@@ -1,4 +1,4 @@
-import { configure, mount } from 'enzyme';
+import { configure, mount, render, shallow } from 'enzyme';
 import React from 'react';
 import axios from 'axios';
 import Adapter from 'enzyme-adapter-react-16';
@@ -6,7 +6,12 @@ import App from '../client/components/App.jsx';
 
 configure({ adapter: new Adapter() });
 
-it('Header should exists', () => {
-  const wrapper = mount(<App />);
-  expect(wrapper.find('h1').text()).toEqual('More places to stay');
+describe('<App />', () => {
+  it('App should have a header should exists', () => {
+    expect(mount(<App />).find('h1').text()).toEqual('More places to stay');
+  });
 });
+
+
+
+
